60f5a930379bb3b2c67fc4c0f002bb65a1724c8c
Adanya Kiran <adanyakiran9701@gmail.com>
2024-07-18 21:18:33 +0530

    Add files via upload

---
+++ AboutComputer.java
@@ -0,0 +1,25 @@
+package firstproject;
+
+public class AboutComputer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("A computer is an electronic device that processes information and provides output.");
+		System.out.println("The word “computer” originates from the Latin word “Computare,” meaning “to calculate.”");
+		System.out.println("Computers have evolved from solving programs and calculations to performing various tasks.\r\n"
+				+ "");
+		System.out.println("Components of a computer include a monitor, mouse, CPU, and keyboard.\r\n"
+				+ "");
+		System.out.println("Computers take input, process data, and generate new information as output.");
+		System.out.println("They are used for creating software, documents, invoices, and lists.");
+		System.out.println("Computers also serve entertainment purposes like gaming, music, and movie playback.");
+		System.out.println("There are three basic types of computers: hybrid, analog, and digital.");
+		System.out.println("Computers find applications in medical, educational, and research fields.");
+		System.out.println("Overall, computers have made our lives easier and more efficient. ");
+
+		
+
+	}
+
+}
+ 
