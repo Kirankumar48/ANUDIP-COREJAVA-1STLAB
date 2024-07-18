c5ed375b3b805cd72aa812f2ccdc311b84219ed7
Adanya Kiran <adanyakiran9701@gmail.com>
2024-07-18 21:19:41 +0530

    Add files via upload

---
+++ AboutJvm.java
@@ -0,0 +1,25 @@
+package firstproject;
+
+public class AboutJvm {
+
+	public static void main(String[] args) {
+		
+	    System.out.println("A java is java virtual machine and it is a software-based engine");
+	    System.out.println("And it is responsible loading a file");
+	    System.out.println("And it has three parts they are classloader subsystem,Run time data area,Execuition engine");
+	    System.out.println("Its loads the dynamic classes and it has some functions");
+	    System.out.println("They are Linking , Loading and Initialising");
+	    System.out.println("Again the linking function is divided into three parts those are:");
+	    System.out.println("verification , preparation and resolving");
+	    System.out.println("Verication function corrects error bytecode and any error maessage");
+	    System.out.println("And the preparation function allocate memory loacation for the variables");
+	    System.out.println("The Resolving fuction is to reffers to the method area");
+	    System.out.println("The extension classlader loads the ext files");
+	    System.out.println("and the bootstrap classloader laods the rt.jar classes");
+	    System.out.println("And apication class loader loads the applications");
+	    System.out.println("The runtime data area has Heap area, Stack area, Pc registers");
+	    System.out.println("and native method area");
+
+	}
+
+}
