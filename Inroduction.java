36ede7e6f66745d04883bc379d964d5d31f1a622
Adanya Kiran <adanyakiran9701@gmail.com>
2024-07-18 21:17:43 +0530

    Add files via upload

---
+++ Inroduction.java
@@ -0,0 +1,20 @@
+package firstproject;
+
+public class Inroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow, iam kiran kumar");
+		System.out.println("my younger sister name is anitha");
+		System.out.println("my elder sister name is saritha");
+		System.out.println("my father name is srinu");
+		System.out.println("my mother name is aruna");
+		System.out.println("I like to watch sci-fi movies");
+		System.out.println("Reading books is my favoriate hobby");
+		System.out.println("And talking about my eduction qualification im parsuing mca 1st year");
+		System.out.println("i completed degree in bsc computers");
+		System.out.println("i completed intermideate in MPC");
+
+	}
+
+}
